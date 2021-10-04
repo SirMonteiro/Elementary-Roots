55f2af18babf6d6aee5a9e31deb63bf15ef731ab
Gabriel <sm.gabriel007@gmail.com>
2021-10-03 22:18:43 -0300

    Updated video link

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -14,7 +14,7 @@ const Video: NextPage = () => {
       <SEO title="Elementary Roots - Project Video" description="Project Video of Nasa Space Apps Challenge" />
       <Header />
       <Box mt="54px" className={styles.videoBox}>
-        <iframe src="https://www.youtube-nocookie.com/embed/ACgTZ01d9O0?modestbranding=1&rel=0&cc_load_policy=1&fs=0&showinfo=0" width="100%" height="100%" frameBorder="0" />
+        <iframe src="https://www.youtube-nocookie.com/embed/Z1xhLvmFng4?modestbranding=1&rel=0&cc_load_policy=0&fs=0&showinfo=0" width="100%" height="100%" frameBorder="0" />
       </Box>
       <Footer />
     </main>
